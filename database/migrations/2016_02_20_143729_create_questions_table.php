962a6023f479bb99c94381b5428a7bf0fd1fe174
Muhammet Faik <muhammetfaik@gmail.com>
2016-02-20 17:10:52 +0200

    created migrations files

---
+++ database/migrations/2016_02_20_143729_create_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('text');
+            $table->integer('owner_id');
+            $table->string('topic');
+            $table->integer('image_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('questions');
+    }
+}
